b678e5635b98d2a9cc011b35e97db458c18ed3d2
alexanderdraev <alexanderdraev@gmail.com>
2018-02-26 20:26:41 +0200

    Init

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table = 'posts';
+    protected $fillable = ['title', 'content', 'category_id', 'user_id'];
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comment', 'post_id', 'id');
+    }
+    
+    public function category() {
+        return $this->belongsTo('App\Models\Category');
+    }
+    
+    public function user() {
+        return $this->belongsTo('App\Models\User');
+    }
+    
+    public function getCommentsCountAttribute() {
+        return $this->comments->count();
+    } 
+    
+}
