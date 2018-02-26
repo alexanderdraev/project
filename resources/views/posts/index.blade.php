2325fc003d4338ffde1add3fcedbba9af114fc52
alexanderdraev <alexanderdraev@gmail.com>
2018-02-26 15:06:42 +0200

    Innit commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+
+@section('content')
+	@foreach($posts as $p)
+	<div>
+	{{$p->title}}
+	</div>
+	<div>
+		<img src="{{$p->imagePath ? $p->imagePath : url('images/defaultImage.png')}}" alt="image not found"/>
+	</div>
+	@endforeach
+
+	<div>
+	{{$posts->render()}}
+	</div>
+@endsection
